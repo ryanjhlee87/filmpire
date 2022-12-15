b44a412a524e5a65373174101827abf65e8effda
ryanjhlee87 <ryan0873035734@gmail.com>
2022-12-15 15:08:29 +0900

    Add Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+
+import useStyles from './styles';
+
+import { Navbar, Movies, MovieInformation, Profile, Actors } from './';
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>App</h1>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/" component={Movies} />
+          <Route exact path="/movie/:id" component={MovieInformation} />
+          <Route exact path="/actors/:id" component={Actors} />
+          <Route exact path="/profile/:id" component={Profile} />
+        </Switch>
+      </main>
     </div>
   );
 };
